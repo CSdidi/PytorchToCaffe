9ddb9b0212828e20cd392bbe60e4d0491936fb9d
wudi <wudi@momenta.ai>
2019-11-05 18:27:17 +0800

    [wudi/dev] add eval_caffe.py

--- example/eval_caffe.py
+++ example/eval_caffe.py
@@ -1,89 +1,44 @@
-# 2018.09.06 by Shining 
-import sys
-#sys.path.insert(0,'/home/shining/Projects/github-projects/caffe-project/caffe/python')
-import caffe
-import torchvision.transforms as transforms
 import numpy as np
-import argparse
-import time
+import sys
+import caffe
 
-import cv2
-
+# Set the right path to your model definition file, pretrained model weights,
+# and the image you would like to classify.
+prototxt = 'models/tfc_deploy.prototxt'
+model = 'models/tfc.caffemodel'
 test_f = 'data/test_6k.npy'
-protofile = 'model/tfc.prototxt'
-weightfile = 'model/tfc.caffemodel'
-net = caffe.Net(protofile,weightfile,caffe.TEST)
 
-#caffe load formate
-def load_image_caffe(imgfile):
-    image = caffe.io.load_image(imgfile)
-    transformer = caffe.io.Transformer({'data': (1, 3, args.height, args.width)})
-    transformer.set_transpose('data', (2, 0, 1))
-    transformer.set_mean('data', np.array([args.meanB, args.meanG, args.meanR]))
-    transformer.set_raw_scale('data', args.scale)
-    transformer.set_channel_swap('data', (2, 1, 0))
+# load the model
+caffe.set_mode_gpu()
+caffe.set_device(0)
+net = caffe.Net(prototxt, model,caffe.TEST)
+print("successfully loaded model")
 
-    image = transformer.preprocess('data', image)
-    image = image.reshape(1, 3, args.height, args.width)
-    return image
+# load test data
+test_data = np.load(test_f)
+print("----type----")
+print(type(test_data))
+print("----shape----")
+print(test_data.shape)
 
-def load_image_pytorch(imgfile):
-    img = np.ones([1,3,args.height, args.width])
-    # 转化为numpy.ndarray并显示
-    return img
-
-
-# Reference from:
-def forward_caffe(protofile, weightfile, image):
-    if args.cuda:
-        caffe.set_device(0)
-        caffe.set_mode_gpu()
+# eval
+correct = 0
+rows = test_data.shape[0]
+cols = test_data.shape[1]
+for i in range(0, rows):
+    line = test_data[i]
+    pred = line[cols-1]
+    input = np.delete(line, -1)
+    input = input.reshape(1,cols-1,1,1)
+    net.blobs['Data1'].data[...] = input
+    out = net.forward()
+    target = out['Softmax1'].argmax()
+    if target != pred:
+        print('idx:', i, 'data:', line, ' target:{}'.format(target), out['Softmax1'][0][target], ' pred:', pred)
     else:
-        caffe.set_mode_cpu()
-    net = caffe.Net(protofile, weightfile, caffe.TEST)
-    net.blobs['blob1'].reshape(1, 3, args.height, args.width)
-    net.blobs['blob1'].data[...] = image
-    t0 = time.time()
-    output = net.forward()
-    t1 = time.time()
-    return t1-t0, net.blobs, net.params
+        correct += 1
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='convert caffe to pytorch')
-    parser.add_argument('--protofile', default='/home/shining/Projects/github-projects/pytorch-project/nn_tools/MGN.prototxt', type=str)
-    parser.add_argument('--weightfile', default='/home/shining/Projects/github-projects/pytorch-project/nn_tools/MGN.caffemodel', type=str)
-    parser.add_argument('--model', default="/home/shining/Projects/github-projects/pytorch-project/nn_tools/model_100.pt", type=str)
-    parser.add_argument('--imgfile', default='/home/shining/Projects/github-projects/pytorch-project/nn_tools/001763.jpg', type=str)
-    parser.add_argument('--height', default=384, type=int)
-    parser.add_argument('--width', default=128, type=int)
-    parser.add_argument('--meanB', default=104, type=float)
-    parser.add_argument('--meanG', default=117, type=float)
-    parser.add_argument('--meanR', default=123, type=float)
-    parser.add_argument('--scale', default=255, type=float)
-    parser.add_argument('--synset_words', default='', type=str)
-    parser.add_argument('--cuda', action='store_true', help='enables cuda')
-
-    args = parser.parse_args()
-    print(args)
-    
-    
-    protofile = args.protofile
-    weightfile = args.weightfile
-    imgfile = args.imgfile
-
-    image = load_image_pytorch(imgfile)
-    time_pytorch, pytorch_blobs, pytorch_models,out_Tensor_pytorch = forward_pytorch(args.model, image)
-    time_caffe, caffe_blobs, caffe_params,out_Tensor_caffe = forward_caffe(protofile, weightfile, image)
-
-    print('pytorch forward time %d', time_pytorch)
-    print('caffe forward time %d', time_caffe)
-    
-    print('------------ Output Difference ------------')
-    blob_name = "cat_blob1"
-    if args.cuda:
-        pytorch_data = pytorch_blobs.data.cpu().numpy().flatten()
-    else:
-        pytorch_data = pytorch_blobs.data.numpy().flatten()
-    caffe_data = caffe_blobs[blob_name].data[0][...].flatten()
-    diff = abs(pytorch_data - caffe_data).sum()
-    print('%-30s pytorch_shape: %-20s caffe_shape: %-20s output_diff: %f' % (blob_name, pytorch_data.shape, caffe_data.shape, diff/pytorch_data.size))
+# print result
+print('\nTest set: Accuracy: {}/{} ({:.4f}%)\n'.format(
+    correct, rows,
+    100. * correct / rows))
